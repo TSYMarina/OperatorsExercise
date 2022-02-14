80a6ebf4e7f9e475ecd96e158c915386c3caabd1
Marina Tsymlianskaia <marinatsy16@gmail.com>
2022-02-14 10:52:34 -0700

    * OperatorsExercise.sln: first commit * Program.cs: first commit * OperatorsExercise.csproj:  first commit
    * .gitignore: first commit

---
+++ OperatorsExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OperatorsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! We are working with two chosen numhbers today: 17 and 4.");
+
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient}, remainder {remainder}");
+            }
+        }
+
+    }
+}
